c21eb3ecf2e45e7e953e57d29679dd7261a853c1
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-03-20 12:46:39 +0530

    added reset password route for users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const {
   userRegister,
   userLogin,
+  resetPassword,
 } = require("../controllers/user.controllers");
+const { auth } = require("../middlewares/auth.middlewares");
 
 const router = express.Router();
 
 router.route("/register").post(userRegister);
 router.route("/login").post(userLogin);
+router.route("/user/:id/reset").post(auth, resetPassword);
 
 module.exports = router;
