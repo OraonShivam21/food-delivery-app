db6b2a39a725ac89355c9e2641f201d6ee28f33e
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-03-20 12:47:45 +0530

    added user routes in server file

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const userRoutes = require("./routes/user.routes");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// using express.json to get the json data
+app.use(express.json());
+
 app.get("/api", (req, res) => {
   res.status(200).json({ message: "Welcome to Food Delivery App API!" });
 });
+
+// use all the routes
+app.get("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
